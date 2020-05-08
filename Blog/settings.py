195972de0e1ca0468e058b6977a79a73c037ccdd
Mavalliss <a.m.popugaev@gmail.com>
2020-05-08 17:49:17 +0300

    Remove secret key from.

--- Blog/settings.py
+++ Blog/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*od_pnnb%am5=0l#n*&53ju!(w%h*jrr1r_y!--t0%xp+hj7xw'
+SECRET_KEY = 'REALLY SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
